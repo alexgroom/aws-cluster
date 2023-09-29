1b3d19877bb4a955c91429020a5114468df2ebbf
Alex Groom <alex.groom@gmail.com>
2023-09-29 17:10:37 +0100

    First commit

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+if [ -z $1 ]
+then
+  echo "Need to supply a cluster name parameter"
+  exit
+fi
+echo Set up AWS Credentials
+aws configure
+echo Create basic OpenShift install config files
+./openshift-install create install-config --dir $1-install-dir
+#
+# edit the install config to add the cluster sizing
+#
+echo Apply worker node config
+yq eval -i '.compute[] |= load("install-platform-worker")' $1-install-dir/install-config.yaml
+echo Apply master node config
+yq eval -i '.controlPlane.platform |= load("install-platform-master")' $1-install-dir/install-config.yaml
+
+echo Take a copy of the install config file
+cp $1-install-dir/install-config.yaml $1-install-dir/install-config-copy
+
+echo *********** Invoke the OpenShift installer, this will take > 30 minutes to complete *****
+./openshift-install create cluster --dir $1-install-dir
+
+echo *********** Install complete **********************
+echo Now login to the cluster using oc and kubeadmin
+
+API=$(yq eval '.clusters[].cluster.server' $1-install-dir/auth/kubeconfig)
+
+oc login ${API} --insecure-skip-tls-verify=true -u kubeadmin -p $(cat $1-install-dir/auth/kubeadmin-password)
+
+oc status
+
+echo *********** Apply LetsEncrypt certs **********************
+
+cd replace_certs
+./certs.sh
+cd ../..
+
+echo *********** Wait for cluster to update **********************
+oc get co
---
+++ replace_certs/certs.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+export RHOCP_API=$(oc whoami --show-server | cut -f 2 -d ':' | cut -f 3 -d '/' | sed 's/-api././')
+export RHOCP_WILDCARD=$(oc get ingresscontroller default -n openshift-ingress-operator -o jsonpath='{.status.domain}')
+
+if [ -d acme.sh]
+then
+  cd acme.sh
+elif
+  git clone https://github.com/acmesh-official/acme.sh.git
+  cp dns_aws.sh acme.sh/dnsapi/ 
+  cd acme.sh
+fi
+
+./acme.sh --issue -d ${RHOCP_API} -d "*.${RHOCP_WILDCARD}" --dns dns_aws
+
+sleep 2
+
+./acme.sh --install-cert -d ${RHOCP_API} -d "*.${RHOCP_WILDCARD}" --cert-file cert.pem --key-file key.pem --fullchain-file fullchain.pem --ca-file ca.cer
+
+cd ..
+
+ls -la 
+
+sleep 2
+
+oc create configmap custom-ca --from-file=ca-bundle.crt=ca.cer -n openshift-config
+
+oc patch proxy/cluster --type=merge --patch='{"spec":{"trustedCA":{"name":"custom-ca"}}}'
+
+oc create secret tls le --cert=fullchain.pem --key=key.pem -n openshift-ingress
+
+oc patch ingresscontroller.operator default --type=merge -p '{"spec":{"defaultCertificate": {"name": "le"}}}' -n openshift-ingress-operator
